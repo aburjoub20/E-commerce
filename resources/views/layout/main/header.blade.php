f96f5c623b5267da730e9935f51174dbcb5495fe
aburjoub20 <proahmadrjoub@gmail.com>
2023-06-11 09:22:13 +0300

    migrate with Dashboard

---
+++ resources/views/layout/main/header.blade.php
@@ -0,0 +1,27 @@
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, shrink-to-fit=no">
+    <title>Admin Dashboard</title>
+    <link rel="icon" type="image/x-icon" href="{{asset('AdminAssets/assets/img/favicon.ico')}}"/>
+    <link href="{{asset('AdminAssets/css/loader.css')}}" rel="stylesheet" type="text/css" />
+
+    <script src="{{asset('AdminAssets/JS/loader.js')}}"></script>
+
+    <!-- BEGIN GLOBAL MANDATORY STYLES -->
+    <link href="https://fonts.googleapis.com/css?family=Nunito:400,600,700" rel="stylesheet">
+    <link href="{{ asset('AdminAssets/css/bootstrap.min.css') }}" rel="stylesheet" type="text/css" />
+    <link href="{{ asset('AdminAssets/css/plugins.css') }}" rel="stylesheet" type="text/css" />
+    <!-- END GLOBAL MANDATORY STYLES -->
+
+    <!-- BEGIN PAGE LEVEL PLUGINS/CUSTOM STYLES -->
+    <link href="{{ asset('AdminAssets/plugins/src/apex/apexcharts.css')}}" rel="stylesheet" type="text/css">
+    <link href="{{ asset('AdminAssets/assets/css/light/dashboard/dash_1.css')}}" rel="stylesheet" type="text/css" />
+    <!-- END PAGE LEVEL PLUGINS/CUSTOM STYLES -->
+
+</head>
+<body class="layout-boxed">
+    <!-- BEGIN LOADER -->
+    <div id="load_screen"> <div class="loader"> <div class="loader-content">
+        <div class="spinner-grow align-self-center"></div>
+    </div></div></div>
